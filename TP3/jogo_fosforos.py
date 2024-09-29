940a4af901279ff6ab4c1021ce8a08c9896955d7
accarol <carolina.gui2005@gmail.com>
2024-09-29 16:59:50 +0100

    Create jogo_fosforos.py

---
+++ TP3/jogo_fosforos.py
@@ -0,0 +1,37 @@
+modo = int(input("Bem vindo(a)! Quem começa a jogar: 1. Utilizador ou 2. Computador?"))
+fosforos = 21
+import random
+if modo == 1:
+    while fosforos > 1:
+        jogada_utilizador = int(input(f"Há {fosforos} fósforos. Quantos deseja retirar? (1-4)"))
+        while jogada_utilizador < 1 or jogada_utilizador > 4 or jogada_utilizador > fosforos:
+            jogada_utilizador = int(input(f"Jogada inválida! Há {fosforos} fósforos. Quantos deseja retirar? (1-4)"))
+        fosforos = fosforos - jogada_utilizador
+        if fosforos == 1:
+            print("O computador retirou 1 fósforo.")
+            print("O computador perdeu!")
+        jogada_computador = (fosforos - 1) % 5
+        if jogada_computador < 1 or jogada_computador > 4:
+            jogada_computador = random.randint(1,4)
+        print(f"O computador retirou {jogada_computador} fósforo(s).")
+        fosforos = fosforos - jogada_computador
+        if fosforos == 1:
+            jogada_utilizador = int(input(f"Há {fosforos} fósforos. Quantos deseja retirar? (1-4)"))
+            print("Você perdeu!!")
+elif modo == 2:
+    while fosforos > 1:
+        jogada_computador = random.randint(1,4)
+        print(f"O computador retirou {jogada_computador} fósforo(s).")
+        fosforos = fosforos - jogada_computador
+        if fosforos == 1:
+            print("Você perdeu!!")
+        jogada_utilizador = int(input(f"Há {fosforos} fósforos. Quantos deseja retirar? (1-4)"))
+        while jogada_utilizador < 1 or jogada_utilizador > 4 or jogada_utilizador > fosforos:
+            jogada_utilizador = int(input(f"Jogada inválida! Há {fosforos} fósforos. Quantos deseja retirar? (1-4)"))
+        fosforos = fosforos - jogada_utilizador
+        if fosforos == 1:
+            print("O computador retirou 1 fósforo.")
+            print("O computador perdeu!")
+else:
+    print("Modo inválido.")
+    modo = int(input("Bem vindo(a)! Quem começa a jogar: 1. Utilizador ou 2. Computador?"))
