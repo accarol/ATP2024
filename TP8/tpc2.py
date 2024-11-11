cf9aadeb43d5e0697355cc04adcaad45aa3766c5
accarol <carolina.gui2005@gmail.com>
2024-11-11 19:36:11 +0000

    Update tpc2.py

--- TP8/tpc2.py
+++ TP8/tpc2.py
@@ -1,1 +1,16 @@
+# a) Especifique uma função que dada uma string e uma substring não vazia, calcula  o número de vezes em que a substring aparece na string, sem que haja sobreposição de substrings:
+
+
+
+# b) Especifique uma função que recebe uma lista de números inteiros positivos e devolve o menor produto que for possível calcular multiplicando os 3 menores inteiros da lista:
+
+
+
+# c) Especifique uma função que dado um número inteiro positivo, repetidamente adiciona os seus dígitos até obter apenas um dígito que é retornado como resultado:
+
+
+
+# d) Especifique uma função que recebe duas strings, `string1` e `string2`, e devolve o índice da primeira ocorrência de `string2` em `string1`, caso não ocorra nenhuma vez a função deverá retornar `-1`:
+
+
 
