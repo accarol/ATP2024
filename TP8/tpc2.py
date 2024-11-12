149ee78daa1d31c86135d840eaf098bde3d03b37
accarol <carolina.gui2005@gmail.com>
2024-11-12 00:19:10 +0000

    Update tpc2.py

--- TP8/tpc2.py
+++ TP8/tpc2.py
@@ -1,10 +1,46 @@
 # a) Especifique uma função que dada uma string e uma substring não vazia, calcula  o número de vezes em que a substring aparece na string, sem que haja sobreposição de substrings:
 
+def strCount(s, subs):
+    res = 0
+    i = 0
+    while i < len(s):
+        if s[i:i+len(subs)] == subs:
+            res = res + 1
+            i = i + len(subs)
+        else:
+            i = i + 1
+    return res
 
+print(strCount("catcowcat", "cat"))
+print(strCount("catcowcat", "cow"))
+print(strCount("catcowcat", "dog"))
 
 # b) Especifique uma função que recebe uma lista de números inteiros positivos e devolve o menor produto que for possível calcular multiplicando os 3 menores inteiros da lista:
 
+def produtoM3(lista):
+    menor1 = lista[0]
+    menor2 = lista[1]
+    menor3 = lista[2]
+    if menor1 > menor2:
+        menor1, menor2 = menor2, menor1
+    if menor2 > menor3:
+        menor2, menor3 = menor3, menor2
+    if menor1 > menor2:
+        menor1, menor2 = menor2, menor1
+    for x in lista[3:]:
+        if x < menor1:
+            menor3 = menor2
+            menor2 = menor1
+            menor1 = x
+        elif x < menor2:
+            menor3 = menor2
+            menor2 = x
+        elif x < menor3:
+            menor3 = x
+    mprod = menor1*menor2*menor3
+    return mprod
 
+print(produtoM3([12,3,7,10,12,8,9]))
 
 # c) Especifique uma função que dado um número inteiro positivo, repetidamente adiciona os seus dígitos até obter apenas um dígito que é retornado como resultado:
 
