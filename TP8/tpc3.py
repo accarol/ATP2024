d7a4165261e7876b9da5d91e9766e48d5366bd11
accarol <carolina.gui2005@gmail.com>
2024-11-13 17:43:15 +0000

    Update tpc3.py

--- TP8/tpc3.py
+++ TP8/tpc3.py
@@ -1,1 +1,27 @@
+# a) `quantosPost`, que indica quantos posts estão registados:
+
+def quantosPost(redeSocial):
+    return len(post)
+
+# b) `postsAutor`, que devolve a lista de posts de um determinado autor:
+
+
+
+# c) `autores`, que devolve a lista de autores de posts ordenada alfabeticamente:
+
+
+
+# d) `insPost`, que acrescenta um novo post à rede social a partir dos parâmetros recebidos e devolve a nova rede social. 
+
+
+
+# e) `remPost`, que remove um post da rede, correspondente ao `id` recebido.
+
+
+
+# f) `postsPorAutor`, que devolve uma distribuição de posts por autor (à semelhança do que foi feito nas aulas).
+
+
+
+# g) `comentadoPor`, que recebe um autor e devolve a lista de posts comentados por esse autor.
 
